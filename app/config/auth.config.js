b32668256fc1bd8ed51fc2d58490cf4d71f71756
shine0901 <truong.quang.hien.93@gmail.com>
2020-10-05 00:28:11 +0700

    first commit

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: 'shine0901-secret-key',
+};
---
+++ app/config/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  url:
+    'mongodb+srv://shine:Good4sk@cluster0-uigtw.mongodb.net/jwt-page?retryWrites=true&w=majority',
+};
